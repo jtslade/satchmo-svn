1ffeb4c8152cdda9d575f25bac79fa40a6b30b55
chris <chris@a38d40e9-c014-0410-b785-c606c0c8e7de>
2006-06-11 02:48:07 +0000

    Adding payment models
    git-svn-id: svn://satchmoproject.com/trunk@6 a38d40e9-c014-0410-b785-c606c0c8e7de

---
+++ payment/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+PAYMENTCHOICES = (
+ ('CreditCard','CreditCard'),
+)
+
+
+class PaymentOption(models.Model):
+    description = models.CharField(maxlength=20)
+    active = models.BooleanField(help_text="Should this be displayed as an option for the user?")
+    optionName = models.CharField(maxlength=20, choices=PAYMENTCHOICES, unique=True, help_text="The class name as defined in payment.py")
+    sortOrder = models.IntegerField()
+    
+    class Admin:
+        list_display = ['optionName','description','active']
+        ordering = ['sortOrder']
+        
+    # Need to validate that each optionName
