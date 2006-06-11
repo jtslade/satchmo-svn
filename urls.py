2b77da53fc0d40b4a16d6d78b0b752d1ccf04ad5
chris <chris@a38d40e9-c014-0410-b785-c606c0c8e7de>
2006-06-11 19:18:22 +0000

    More file removal
    git-svn-id: svn://satchmoproject.com/trunk@15 a38d40e9-c014-0410-b785-c606c0c8e7de

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    # Example:
-    # (r'^satchmo/', include('satchmo.apps.foo.urls.foo')),
-
-    # Uncomment this for admin:
-     (r'^admin/', include('django.contrib.admin.urls')),
-)
