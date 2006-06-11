267729435681c27d8670f6ffd4b274920559aa43
chris <chris@a38d40e9-c014-0410-b785-c606c0c8e7de>
2006-06-11 18:05:44 +0000

    Updated the shop config model to have more examples of types of info we need to collect.
    git-svn-id: svn://satchmoproject.com/trunk@11 a38d40e9-c014-0410-b785-c606c0c8e7de

--- shop/models.py
+++ shop/models.py
@@ -2,8 +2,20 @@ from django.db import models
 
 
 class Config(models.Model):
-    storeName = models.CharField("Store Name",maxlength=100)
-    
+    """
+    Used to store specific information about a store.  Also used to 
+    configure various store behaviors
+    """
+    storeName = models.CharField("Store Name",maxlength=100, unique=True)
+    storeDescription = models.TextField(blank=True)
+    street1=models.CharField("Street",maxlength=50)
+    street2=models.CharField("Street", maxlength=50, blank=True)
+    city=models.CharField("City", maxlength=50)
+    state=models.USStateField("State")
+    zip_code=models.CharField("Zip Code", maxlength=9)
+    country=models.CharField("Country", maxlength=50, blank=True)
+    phone = models.PhoneNumberField(blank=True)
+    noStockCheckout = models.BooleanField("Purchase item not in stock?")
     
     def __str__(self):
         return self.storeName
