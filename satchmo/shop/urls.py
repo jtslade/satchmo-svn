a5cf027ec436b24e6bef2b42a3b17d678a0c8acf
chris <chris@a38d40e9-c014-0410-b785-c606c0c8e7de>
2006-07-14 02:45:21 +0000

    Added category structure to the urls. Started to add breadcrumb capability
    git-svn-id: svn://satchmoproject.com/trunk@34 a38d40e9-c014-0410-b785-c606c0c8e7de

--- satchmo/shop/urls.py
+++ satchmo/shop/urls.py
@@ -5,9 +5,13 @@ from django.conf import settings
 
 urlpatterns = patterns('satchmo.shop.views',
      (r'^$', 'index'),
-     (r'^product/(?P<slug>[-\w]+)/$','product'), 
+     (r'^product/(?P<slug>[-\w]+)/$','product'),
+     (r'^category/(?P<slug>[-\w]+)/$','category_root'),
+     (r'^category/(?P<slug_parent>[-\w]+)/(?P<slug>[-\w]+)/$','category_children'),
+     (r'^category/([-\w]+/)+(?P<slug_parent>[-\w]+)/(?P<slug>[-\w]+)/$','category_children')
 )
-
+#Note with the last url - this allows category depth to be as deep as we want but the downside
+#is that we ignore all but the child and parent category.  In practice this should be ok
 if settings.LOCAL_DEV:
     urlpatterns += patterns('',
         (r'^site_media/(.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
--- satchmo/shop/views.py
+++ satchmo/shop/views.py
@@ -1,7 +1,8 @@
 # Create your views here.
 
 from django.shortcuts import render_to_response
-from satchmo.product.models import Item
+from satchmo.product.models import Item, Category
+
 
 def index(request):
     featured_items = Item.objects.filter(active="1").filter(featured="1")
@@ -10,3 +11,12 @@ def index(request):
 def product(request, slug):
     item = Item.objects.filter(active="1").filter(short_name=slug)[0]
     return render_to_response('base_product.html',{'item':item})
+        
+def category_root(request, slug):
+    category = Category.objects.filter(slug=slug)[0]
+    return render_to_response('base_category.html',{'category':category})
+
+def category_children(request, slug_parent, slug):
+    parent = Category.objects.filter(slug=slug_parent)[0]
+    category = parent.child.filter(slug=slug)[0]
+    return render_to_response('base_category.html',{'category':category})
