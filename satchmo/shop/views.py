5d345d3fabf7a8b2f61648a8299b81ccc9bf33a4
chris <chris@a38d40e9-c014-0410-b785-c606c0c8e7de>
2006-07-15 01:42:36 +0000

    Added better 404 handling for categories and products that don't exist.
    git-svn-id: svn://satchmoproject.com/trunk@35 a38d40e9-c014-0410-b785-c606c0c8e7de

--- satchmo/shop/views.py
+++ satchmo/shop/views.py
@@ -1,7 +1,18 @@
 # Create your views here.
 
 from django.shortcuts import render_to_response
+from django import http
+from django.template import RequestContext, loader
 from satchmo.product.models import Item, Category
+
+def bad_or_missing(request, msg):
+    """
+    Return an HTTP 404 response for a date request that cannot possibly exist.
+    The 'msg' parameter gives the message for the main panel on the page.
+    """
+    template = loader.get_template('shop_404.html')
+    context = RequestContext(request, {'message': msg})
+    return http.HttpResponseNotFound(template.render(context))
 
 
 def index(request):
@@ -9,14 +20,31 @@ def index(request):
     return render_to_response('base_index.html', {'all_items': featured_items})
 
 def product(request, slug):
-    item = Item.objects.filter(active="1").filter(short_name=slug)[0]
+    try:
+        item = Item.objects.filter(active="1").get(short_name=slug)
+    except Item.DoesNotExist:
+        return bad_or_missing(request, 'The product you have requested does '
+                'not exist.')
     return render_to_response('base_product.html',{'item':item})
         
 def category_root(request, slug):
-    category = Category.objects.filter(slug=slug)[0]
+    try:
+        category = Category.objects.filter(slug=slug)[0]
+    except IndexError:
+        return bad_or_missing(request, 'The category you have requested does '
+            'not exist.')
     return render_to_response('base_category.html',{'category':category})
 
 def category_children(request, slug_parent, slug):
-    parent = Category.objects.filter(slug=slug_parent)[0]
-    category = parent.child.filter(slug=slug)[0]
+    try:
+        parent = Category.objects.filter(slug=slug_parent)[0]
+    except IndexError:
+        return bad_or_missing(request, 'The category you have requested does '
+            'not exist.')
+    try:
+        category = parent.child.filter(slug=slug)[0]
+    except IndexError:
+        return bad_or_missing(request, 'The category you have requested does '
+            'not exist.')
+            
     return render_to_response('base_category.html',{'category':category})
