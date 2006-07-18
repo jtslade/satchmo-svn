79af336b26e52f624297bb962a6532371515b42d
GinTon <GinTon@a38d40e9-c014-0410-b785-c606c0c8e7de>
2006-07-18 20:36:37 +0000

    Improved to working with objects related.
    git-svn-id: svn://satchmoproject.com/trunk@44 a38d40e9-c014-0410-b785-c606c0c8e7de

--- satchmo/load_localization.py
+++ satchmo/load_localization.py
@@ -7,145 +7,75 @@ import sys
 from satchmo.localization.models import *
 
 
-"""
-Country
--------
-The USPS primary destinations are formed by the country code, a dot,
-and a code invented fot that localization. Setting off 'display'
-
-"""
-
-# Configuration
-# ---------------------------------------------------------------------- #
-DATA_DIR = "./data"  # CSV files.
-MODELS_FILE = "./localization/models.py"
-
-# Countries unhabitted or with a very few habitants. Setting off 'display'
-UNHABITTED = ['AQ', 'BV', 'HM', 'PN', 'GS', 'UM']
-# ---------------------------------------------------------------------- #
-
-
-def get_data_models():
+def get_data_models(models_file):
     '''Get the name of all models and fields.
-Add '#' character after of field name if is numeric.
+And add information about numeric fields and keys.
+
+The numeric fields have as format:
+#[numeric X position]...
+
+And the keys:
+:K-[num. pos. of key],[model related]
     '''
-    global FULL_MODELS
-    FULL_MODELS = []
-    model_data = []
+    list_models = []
+    model = []
+    pos_numeric = []  # Position of numeric fields
+    info_keys = []  # Info. about keys
     re_field = re.compile('\s+\w+\s*=\s*models\.')  # Line with field name
     re_class = re.compile('\s+class ')  # For Admin and Meta
     re_def = re.compile('\s+def ')
     is_new_model = False
 
-    for line in open(MODELS_FILE):
+    for line in open(models_file):
         # The models start with 'class'
         if not is_new_model and line.startswith('class'):
-            FULL_MODELS.append(model_data)
             model_name = line.replace('class','').split('(')[0].strip()
-            model_data.append(model_name)
+            model.append(model_name)
             is_new_model = True
         elif is_new_model:
             if re_field.match(line):
                 field_name = line.split('=')[0].strip()
-                model_data.append(field_name)
+                model.append(field_name)
+
                 if 'models.FloatField' in line or 'IntegerField' in line:
-                    model_data.append('#')  # To indicating that is numeric.
+                    pos_numeric.append(len(model)-2)  # Discard model name.
+                elif 'models.ForeignKey' in line:
+                    key_name = line.split('(')[-1].strip().strip(')')
+                    position = len(model)-2  # Discard model name.
+                    info_keys.append(':')
+                    info_keys.append(str(position) + ',')
+                    info_keys.append(key_name)
+            # It is supposed that models in localization has at the end:
+            # ('class Meta', 'class Admin', or some 'def')
             elif re_class.match(line) or re_def.match(line):
-                model_data = []
+                if pos_numeric:
+                    pos_num2str = '#'
+                    for num in pos_numeric:
+                        pos_num2str += str(num)
+                    model.append(pos_num2str)
+                    model.append(':N')  # To detect the numeric field.
+                    pos_numeric = []
+                if info_keys:
+                    all_keys = ""
+                    for key in info_keys:
+                        all_keys += key
+                    model.append(all_keys)
+                    model.append(':K')  # To detect fastly some key.
+                    info_keys = []
+                list_models.append(model)
+                model = []
                 is_new_model = False
 
+    return list_models
 
-def load_data(csv_file):
-    '''Get the fields position where there are numbers.
+
+def sort_models(data_dir, list_models):
+    '''The models with keys (foreign, etc) go at the end.
     '''
-    global FULL_MODELS
-    position_num = []  # Store the position of numeric fields
-    fields = []  # Fields name of model
-    re_int = re.compile('\d+$')  # An integer number
-    csv_separator = ':'  # Separator used in CSV data
-
-    # Get data from CSV header.
-    # The model is separated of the fields by ':'
-    first_line = open(csv_file).readline().split(':')
-    model = first_line[0].strip()
-    for field in first_line[-1].split(','):  # Fields separated by ','
-        fields.append(field.strip())
-    fields_number = len(fields)
-
-    # Check that names in CSV header are correct, and check numeric fields.
-    data_found = False
-    for data_model in FULL_MODELS:
-        if data_model[0] == model:
-            len_data_model = len(data_model) - 1
-            for field in fields:
-                if field not in data_model:
-                    sys.exit("Error in file '%s'. \
-Field name '%s' is not correct." % (csv_file, field))
-                # Get the position of each field to check '#' (numeric field)
-                else:
-                    position = data_model.index(field)
-                    # The position has to be lesser that length of list -1
-                    if position < len_data_model and \
-                      data_model[position+1] == '#':
-                        position = fields.index(field)
-                        position_num.append(position)
-
-            data_found = True
-            break
-
-    if not data_found:
-        sys.exit("Error in file '%s'. Model name '%s' is not correct." \
-               % (csv_file, model))
-
-    # Convert from CSV to Django ORM
-    reader = csv.reader(open(csv_file), delimiter=csv_separator)
-    reader.next()  # Skip the header
-
-    line_number = 0
-    for csv_line in reader:
-        object_line = []
-        line_number += 1
-        object_line.append("c%d = %s(" % (line_number, model))
-
-        for position in range(0, fields_number):
-            if csv_line[position]:
-                if not object_line[-1].endswith('('):
-                    object_line.append(', ')
-
-                if position in position_num:  # If the field is an integer
-                    object_line.append('%s=%s' \
-                        % (fields[position], csv_line[position]))
-                else:
-                    object_line.append('%s="%s"' \
-                        % (fields[position], csv_line[position]))
-
-        object_line.append(")")
-        load_object = ''.join(object_line)
-        exec(load_object)  # Load the object
-#        print load_object
-
-    # At the end, save all objects together
-    print "Creating %s objects ..." % model
-    for num in range(1, line_number+1):
-        obj = eval('c%d' % num)
-        if model == 'Country':
-            if obj.alpha2_code in UNHABITTED or '.' in obj.alpha2_code:
-                obj.display = False
-        obj.save()
-
-
-def main():
-    '''Prepopulate the information from CSV data into tables.
-
-If you wish to modify data, modify the files in 'DATA_DIR' directory.
-
-The header format in the CSV files has to be:
-[Model name]: [field 1] [, field 2, ... field n]
-    '''
-    get_data_models()
+    list_new_models = []  # Only left fields in CSV file.
 
     try:
-        os.chdir(DATA_DIR)
+        os.chdir(data_dir)
     except OSError, err:
         print "Error! %s: %r" % (err.strerror, err.filename)
         sys.exit(1)
@@ -155,7 +85,201 @@ The header format in the CSV files has to be:
         sys.exit("Error! Not found CSV files.")
 
     for csv in csv_files:
-        load_data(csv)
+        list_new_models.append(check_header(csv, list_models))
+
+    for model in list_new_models[:]:
+        # Put models with ForeignKey in the last positions
+        if ':K' in model and \
+          list_new_models.index(model) < len(list_new_models)-1:
+            list_new_models.append(model)
+            list_new_models.remove(model)
+
+    return list_new_models
+
+
+def check_header(csv_file, list_models):
+    '''Get and check data from CSV header, create a new list of models and
+insert the file CSV name in model line.
+
+Format: * code: [Model name], [field name 1], ... [field name n]*
+    '''
+    new_model = []
+    line_found = False
+    full_line = False
+
+    # Get line with info. about model.
+    for line in open(csv_file):
+        if line.startswith('#'):
+            if not line_found and '* code:' in line:
+                code_line = line.split(':')[-1]
+                line_found = True
+            elif line_found:
+                if not '*' in code_line:
+                    code_line += line
+                else:
+                    full_line = True
+                    break
+        else:
+            break
+
+    if not line_found:
+        sys.exit("Code line not found in '%s'." % csv_file)
+    if not full_line:
+        sys.exit("Code line not finished with '*' in '%s'." % csv_file)
+
+    code_line = code_line.split('*')[0].replace('#','')
+    # Fields name of model in CSV file separated by ','
+    csv_fields = [ x.strip() for x in code_line.split(',') ]
+    model = csv_fields[0]
+    csv_fields.remove(csv_fields[0])
+
+    # Check header and build new list.
+    for data_model in list_models:
+        if data_model[0] == model:  # Check model name.
+            new_model.append(model)  # Add model name
+            for field in csv_fields:
+                if field in data_model:  # Check fields.
+                    new_model.append(field)  # Add field.
+                else:
+                    sys.exit("Error in file '%s'. \
+Field name '%s' is not correct." % (csv_file, field))
+
+            # Insert name of CSV file
+            new_model.insert(0, csv_file)
+            # Insert both numeric and key fields
+            new_model += [ x for x in data_model if '#' in x or ':' in x ]
+            break
+
+    if not new_model:
+        sys.exit("Error in file '%s'. Model name '%s' is not correct." \
+               % (csv_file, model))
+
+    return new_model
+
+
+def comment_stripper(iterator):
+    '''Generator that filters comments and blank lines.
+
+Used as input to 'cvs.reader'.
+    '''
+    for line in iterator:
+        if line [:1] == '#':
+            continue
+        if not line.strip ():
+            continue
+        yield line
+
+
+def load_data(model):
+    '''Get the fields position where there are numbers.
+    '''
+    position_num = []
+    dic_keys = {}
+    csv_separator = ':'
+    re_num = re.compile('\d+$')
+    # Countries unhabitted or with a very few habitants. Setting off 'display'
+    unhabitted = ['AQ', 'BV', 'HM', 'PN', 'GS', 'UM']
+
+    # Get only the fields name
+    fields = [ x for x in model[2:] if not (':' in x or '#' in x) ]
+    # Get left data.
+    fields_number = len(fields)
+    csv_file = model[0]
+    model_name = model[1]
+
+    # Get the position of numeric fields
+    if ':N' in model:
+        pos = model.index(':N')
+        position_num = model[pos-1]  # The field numeric is before of ':N'
+        position_num = [ int(x) for x in position_num if not '#' in x ]
+
+    # Info. about keys
+    if ':K' in model:
+        pos = model.index(':K')
+        info_keys = model[pos-1]
+        # Format-> :[position],[model name]:...
+        info_keys = info_keys.split(':')[1:]
+        keys = [ (int(x.split(',')[0]), x.split(',')[1]) for x in info_keys ]
+        dic_keys = dict(keys)
+
+    # Convert from CSV to Django ORM
+    reader = csv.reader(comment_stripper(
+                        open(csv_file)), delimiter=csv_separator)
+
+    id_model = ''
+    line_number = 0
+    for csv_line in reader:
+        object_line = []
+        key_line = []
+        line_number += 1
+        object_line.append("c%d = %s(" % (line_number, model_name))
+
+        for position in range(0, fields_number):
+            # If the field is not empty
+            if csv_line[position]:
+                if not object_line[-1].endswith('('):
+                    object_line.append(', ')
+                # If is a key
+                if dic_keys and dic_keys.has_key(position):
+                    object_line.append('%s=id_key' % fields[position])
+                    key_model = dic_keys.get(position)
+
+                    if csv_line[position] != id_model:
+                        id_model = csv_line[position]
+
+                        key_line.append('id_key = %s.objects.get(pk=' \
+                                        % key_model)
+                        if re_num.match(id_model):  # integer
+                            key_line.append('%d)' % id_model)
+                        else:
+                            key_line.append('"%s")' % id_model)
+                # If is an integer
+                elif position in position_num:
+                    object_line.append('%s=%s' \
+                        % (fields[position], csv_line[position]))
+                # If is a string.
+                else:
+                    object_line.append('%s="%s"' \
+                        % (fields[position], csv_line[position]))
+
+        if key_line:
+            load_key = ''.join(key_line)
+            exec(load_key)
+
+        object_line.append(")")
+        load_object = ''.join(object_line)
+        exec(load_object)  # Load the object
+
+    # At the end, save all objects together
+    print "Creating %s objects ..." % model_name
+    if model_name == 'Country':
+        for num in range(1, line_number+1):
+            obj = eval('c%d' % num)
+            if obj.alpha2_code in unhabitted or '.' in obj.alpha2_code:
+                obj.display = False
+            obj.save()
+    else:
+        for num in range(1, line_number+1):
+            obj = eval('c%d' % num)
+            obj.save()
+
+
+def main():
+    '''Prepopulate the information from CSV data into tables.
+
+If you wish to modify data, modify the files in 'data_dir' directory.
+
+The header format in the CSV files has to be:
+[Model name]: [field 1] [, field 2, ... field n]
+    '''
+    data_dir = "./data"  # CSV files.
+    models_file = "./localization/models.py"
+
+    models = get_data_models(models_file)
+    new_models = sort_models(data_dir, models)
+
+    for model in new_models:
+        load_data(model)
 
 
 if __name__ == '__main__':
