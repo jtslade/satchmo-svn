b4b80e07fe0d76cfc1b246af8e322905887837aa
chris <chris@a38d40e9-c014-0410-b785-c606c0c8e7de>
2006-08-17 02:13:24 +0000

    Update the range function
    git-svn-id: svn://satchmoproject.com/trunk@83 a38d40e9-c014-0410-b785-c606c0c8e7de

--- satchmo/shop/templatetags/template_range.py
+++ satchmo/shop/templatetags/template_range.py
@@ -5,6 +5,6 @@ register = template.Library()
 def template_range(value):
     '''Return a range 1..value'''
     #return range(1,1+value)
-    return range(value)
+    return range(1,value+1)
     
-register.filter('template_range',range)
+register.filter('template_range',template_range)
