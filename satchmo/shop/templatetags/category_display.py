8fc735476e71db17d72c3cc0460fd1292abb5151
chris <chris@a38d40e9-c014-0410-b785-c606c0c8e7de>
2006-11-25 16:53:35 +0000

    Closing #20 to make category_display handle unicode.  Also added capability to hide empty categories
    git-svn-id: svn://satchmoproject.com/trunk@158 a38d40e9-c014-0410-b785-c606c0c8e7de

--- satchmo/shop/templatetags/category_display.py
+++ satchmo/shop/templatetags/category_display.py
@@ -8,22 +8,26 @@ except ImportError:
 
 register = Library()
 
-def recurse_for_children(current_node, parent_node):
+def recurse_for_children(current_node, parent_node, show_empty=True):
     if current_node.child.count() > 0:
         temp_parent = SubElement(parent_node,"li")
         attrs = { 'href' : current_node.get_absolute_url() }
         link = SubElement(temp_parent, "a", attrs)
-        link.text = current_node.name
+        link.text = unicode(current_node.name,'utf-8')
         new_parent = SubElement(temp_parent,"ul")
         children = current_node.child.all()
         for child in children:
             recurse_for_children(child, new_parent)
-    else:
+    elif current_node.item_set.count() > 0:
         temp_parent = SubElement(parent_node,"li")
         attrs = { 'href' : current_node.get_absolute_url() }
         link = SubElement(temp_parent, "a", attrs)
-        link.text = current_node.name
-
+        link.text = unicode(current_node.name,'utf-8') 
+    elif current_node.item_set.count() == 0 and show_empty:
+        temp_parent = SubElement(parent_node,"li")
+        attrs = { 'href' : current_node.get_absolute_url() }
+        link = SubElement(temp_parent, "a", attrs)
+        link.text = unicode(current_node.name,'utf-8')
 
 def category_tree():
     """
@@ -45,6 +49,6 @@ def category_tree():
     for cats in Category.objects.all():
         if not cats.parent:
             recurse_for_children(cats, root)
-    return tostring(root)
+    return tostring(root, 'utf-8')
 
 register.simple_tag(category_tree)
