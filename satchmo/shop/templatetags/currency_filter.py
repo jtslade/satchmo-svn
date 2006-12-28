00a8576bb73f0736e10f9a776b2ae46bde07b935
chris <chris@a38d40e9-c014-0410-b785-c606c0c8e7de>
2006-12-28 03:31:52 +0000

    Added improved currency filter from Python recipe.  Closes out #26
    git-svn-id: svn://satchmoproject.com/trunk@195 a38d40e9-c014-0410-b785-c606c0c8e7de

--- satchmo/shop/templatetags/currency_filter.py
+++ satchmo/shop/templatetags/currency_filter.py
@@ -1,10 +1,67 @@
 from django import template
 from django.conf import settings
+from decimal import Decimal
+
+#The moneyfmt script was taken from the Python decimal recipes.
+#See it here - http://docs.python.org/lib/decimal-recipes.html
 
 register = template.Library()
 
-def currency_formatter(value):
-    """ Return a formatted string for currency"""
-    return ("%s%s" % (settings.CURRENCY, value))
-    
-register.filter('currency',currency_formatter)
+
+def moneyfmt(value, places=2, curr=settings.CURRENCY, sep=',', dp='.',
+             pos='', neg='-', trailneg=''):
+    """Convert Decimal to a money formatted string.
+
+    places:  required number of places after the decimal point
+    curr:    optional currency symbol before the sign (may be blank)
+    sep:     optional grouping separator (comma, period, space, or blank)
+    dp:      decimal point indicator (comma or period)
+             only specify as blank when places is zero
+    pos:     optional sign for positive numbers: '+', space or blank
+    neg:     optional sign for negative numbers: '-', '(', space or blank
+    trailneg:optional trailing minus indicator:  '-', ')', space or blank
+
+    >>> d = Decimal('-1234567.8901')
+    >>> moneyfmt(d, curr='$')
+    '-$1,234,567.89'
+    >>> moneyfmt(d, places=0, sep='.', dp='', neg='', trailneg='-')
+    '1.234.568-'
+    >>> moneyfmt(d, curr='$', neg='(', trailneg=')')
+    '($1,234,567.89)'
+    >>> moneyfmt(Decimal(123456789), sep=' ')
+    '123 456 789.00'
+    >>> moneyfmt(Decimal('-0.02'), neg='<', trailneg='>')
+    '<.02>'
+
+    """
+    q = Decimal((0, (1,), -places))    # 2 places --> '0.01'
+    sign, digits, exp = value.quantize(q).as_tuple()
+    assert exp == -places    
+    result = []
+    digits = map(str, digits)
+    build, next = result.append, digits.pop
+    if sign:
+        build(trailneg)
+    for i in range(places):
+        if digits:
+            build(next())
+        else:
+            build('0')
+    build(dp)
+    i = 0
+    while digits:
+        build(next())
+        i += 1
+        if i == 3 and digits:
+            i = 0
+            build(sep)
+    build(curr)
+    if sign:
+        build(neg)
+    else:
+        build(pos)
+    result.reverse()
+    return ''.join(result)
+
+
+register.filter('currency',moneyfmt)
