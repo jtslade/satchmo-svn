c73463f2f4002b3246d47e7a5c482abe4fbb5a36
chris <chris@a38d40e9-c014-0410-b785-c606c0c8e7de>
2007-04-09 01:58:23 +0000

    Moving around the checkout views so it's easier and clearer to add alternative checkout paths.
    git-svn-id: svn://satchmoproject.com/satchmo/trunk@275 a38d40e9-c014-0410-b785-c606c0c8e7de

--- satchmo/shop/urls.py
+++ satchmo/shop/urls.py
@@ -16,9 +16,9 @@ urlpatterns = patterns('satchmo.shop.views',
      (r'^account/info/$','account.info'),
      (r'^account/logout/$','account.shop_logout'),
      (r'^contact/$','contact.form'),
-     (r'^checkout/$','checkout-step1.contact_info', {'SSL':False}, 'checkout-step1'),
-     (r'^checkout/pay/$','checkout-step2.pay_ship_info', {'SSL':False}, 'checkout-step2'),  
-     (r'^checkout/confirm/$','checkout-step3.confirm_info', {'SSL':False}, 'checkout-step3'),     
+     (r'^checkout/$','credit-card.checkout-step1.contact_info', {'SSL':False}, 'checkout-step1'),
+     (r'^checkout/pay/$','credit-card.checkout-step2.pay_ship_info', {'SSL':False}, 'checkout-step2'),  
+     (r'^checkout/confirm/$','credit-card.checkout-step3.confirm_info', {'SSL':False}, 'checkout-step3'),     
 )
 #Note with the last category url - this allows category depth to be as deep as we want but the downside
 #is that we ignore all but the child and parent category.  In practice this should be ok
