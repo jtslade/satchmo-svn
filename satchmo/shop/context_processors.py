48487df324ec6470f2759740f8390e1d0fe4cefc
chris <chris@a38d40e9-c014-0410-b785-c606c0c8e7de>
2007-04-30 01:36:14 +0000

    Improvement to context processor suggested in #49. Minor tweak to template to take advantage of the new handling of a Null Cart. Thanks Bruce.
    git-svn-id: svn://satchmoproject.com/satchmo/trunk@295 a38d40e9-c014-0410-b785-c606c0c8e7de

--- satchmo/shop/context_processors.py
+++ satchmo/shop/context_processors.py
@@ -4,6 +4,27 @@ http://www.b-list.org/weblog/2006/06/14/django-tips-template-context-processors
 
 It is used to add some common variables to all the templates
 """
+from decimal import Decimal
+ 
+class NullCart(object):
+    """Standin for a real cart when we don't have one yet.  More convenient than testing for null all the time."""
+    desc = None
+    date_time_created = None
+    customer = None
+    total=Decimal("0")
+    numItems=0
+    
+    def add_item(self, chosen_item, number_added):
+        pass
+        
+    def remove_item(self, chosen_item_id, number_removed):
+        pass
+    
+    def empty(self):
+        pass
+        
+    def __str__(self):
+        return "NullCart (empty)"
 
 
 def settings(request):
@@ -21,16 +42,14 @@ def settings(request):
         shop_name = "Test Store"
     if request.session.get('cart',False):
         tempCart = Cart.objects.get(id=request.session['cart'])
-        cart_count = tempCart.numItems
         cart = tempCart
     else:
-        cart_count = 0
-        cart = ''
+        cart = NullCart()
     all_categories = Category.objects.all()
     return {'shop_base': settings.SHOP_BASE,
              'shop_name': shop_name,
              'media_url': settings.MEDIA_URL,
-             'cart_count': cart_count,
+             'cart_count': cart.numItems,
              'cart': cart,
              'categories': all_categories,
              'enable_google': settings.GOOGLE_ANALYTICS}
