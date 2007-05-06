5a39d7086630a0418ff283064c39264dd6365454
chris <chris@a38d40e9-c014-0410-b785-c606c0c8e7de>
2007-05-06 01:35:41 +0000

    Updates to thumbnail code to support recent changes in Django code. Closes ticket #48.
    git-svn-id: svn://satchmoproject.com/satchmo/trunk@300 a38d40e9-c014-0410-b785-c606c0c8e7de

--- satchmo/thumbnail/field.py
+++ satchmo/thumbnail/field.py
@@ -5,45 +5,64 @@ from django.db.models import signals
 
 def _delete(instance=None):
     if instance:
-        print '[thumbnail] DELETE', instance
         remove_model_thumbnails(instance)
-#
 
 class ImageWithThumbnailField(ImageField):
     """ ImageField with thumbnail support
-    
+
         auto_rename: if it is set perform auto rename to
         <class name>-<field name>-<object pk>.<ext>
         on pre_save.
     """
 
-    def __init__(self, verbose_name=None, name=None, width_field=None, height_field=None, auto_rename=True, **kwargs):
+    def __init__(self, verbose_name=None, name=None,
+                 width_field=None, height_field=None,
+                 auto_rename=True, **kwargs):
         self.width_field, self.height_field = width_field, height_field
-        super(ImageWithThumbnailField, self).__init__(verbose_name, name, width_field, height_field, **kwargs)
+        super(ImageWithThumbnailField, self).__init__(verbose_name, name,
+                                                      width_field, height_field,
+                                                      **kwargs)
         self.auto_rename = auto_rename
-    #
-    
+
     def _save(self, instance=None):
         if not self.auto_rename: return
         if instance == None: return
+        if not instance._get_pk_val():
+            # NEW OBJECT, connect to the post_save for renaming
+            dispatcher.connect(self._save_new, signals.post_save, sender=instance)
+            return
         image = getattr(instance, self.attname)
         # XXX this needs testing, maybe it can generate too long image names (max is 100)
         image = rename_by_field(image, '%s-%s-%s' \
-                                     % (instance.__class__.__name__,
-                                         self.name,
-                                         instance._get_pk_val()
-                                        )
+                                % (instance.__class__.__name__,
+                                   self.name,
+                                   instance._get_pk_val()
                                    )
+                                )
         setattr(instance, self.attname, image)
-    #
-    
+
+    def _save_new(self, instance):
+        if not self.auto_rename: return
+        try:
+            image = getattr(instance, self.attname)
+            # XXX this needs testing, maybe it can generate too long image names (max is 100)
+            image = rename_by_field(image, '%s-%s-%s' \
+                                    % (instance.__class__.__name__,
+                                       self.name,
+                                       instance._get_pk_val()
+                                       )
+                                    )
+            setattr(instance, self.attname, image)
+            instance.save()
+        finally:
+            # cleanup
+            dispatcher.disconnect(self._save_new, signals.post_save, sender=instance)
+
     def contribute_to_class(self, cls, name):
         super(ImageWithThumbnailField, self).contribute_to_class(cls, name)
         dispatcher.connect(_delete, signals.post_delete, sender=cls)
         dispatcher.connect(self._save, signals.pre_save, sender=cls)
-    #
 
     def get_internal_type(self):
         return 'ImageField'
-    #
-#
+
