427d54e19f29db107b2972ce496f6602a949a440
jshaffer <jshaffer@a38d40e9-c014-0410-b785-c606c0c8e7de>
2007-05-12 13:09:53 +0000

    Remove stray </div>, add comma in shop/urls.py.
    git-svn-id: svn://satchmoproject.com/satchmo/trunk@310 a38d40e9-c014-0410-b785-c606c0c8e7de

--- satchmo/shop/urls.py
+++ satchmo/shop/urls.py
@@ -23,7 +23,7 @@ urlpatterns += patterns('satchmo.shop.views',
      (r'^checkout/$', 'credit_card.checkout_step1.contact_info', {'SSL':False}, 'satchmo_checkout-step1'),
      (r'^checkout/pay/$', 'credit_card.checkout_step2.pay_ship_info', {'SSL':False}, 'satchmo_checkout-step2'),
      (r'^checkout/confirm/$', 'credit_card.checkout_step3.confirm_info', {'SSL':False}, 'satchmo_checkout-step3'),
-     (r'^checkout/success/$', 'common.checkout_success', {'SSL':False}, 'satchmo_checkout-success')
+     (r'^checkout/success/$', 'common.checkout_success', {'SSL':False}, 'satchmo_checkout-success'),
 )
 #Note with the last category url - this allows category depth to be as deep as we want but the downside
 #is that we ignore all but the child and parent category.  In practice this should be ok
