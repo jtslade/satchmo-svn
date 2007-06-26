014b4d3a2a5e94074455d96f05ab0499f78f397f
jshaffer <jshaffer@a38d40e9-c014-0410-b785-c606c0c8e7de>
2007-06-26 17:24:32 +0000

    Close #15: Complete sitemaps integration.
    git-svn-id: svn://satchmoproject.com/satchmo/trunk@457 a38d40e9-c014-0410-b785-c606c0c8e7de

--- satchmo/shop/views/sitemaps.py
+++ satchmo/shop/views/sitemaps.py
@@ -14,7 +14,43 @@ class ProductSitemap(Sitemap):
     def items(self):
         return Item.objects.filter(active=True)
 
+class MainSitemap(Sitemap):
+    urls = []
+
+    def items(self):
+        return self.urls
+
+    def add_url(self, location, priority=0.5, changefreq='weekly'):
+        self.urls.append({
+            'location': location,
+            'priority': priority,
+            'changefreq': changefreq,
+        })
+
+    def location(self, obj):
+        return obj['location']
+
+    def priority(self, obj):
+        return obj['priority']
+
+    def changefreq(self, obj):
+        return obj['changefreq']
+
+main_urls = (
+    ('/', 1.0, 'hourly'),
+    ('/contact/', 1.0, 'monthly'),
+    ('/cart/', 0.5, 'monthly'),
+    ('/account/login/', 0.8, 'monthly'),
+    ('/account/create/', 0.8, 'monthly'),
+    ('/account/password_reset/', 0.8, 'monthly'),
+)
+
+satchmo_main = MainSitemap()
+for url in main_urls:
+    satchmo_main.add_url(*url)
+
 sitemaps = {
+    'satchmo_main': satchmo_main,
     'category': CategorySitemap,
     'products': ProductSitemap,
 }
