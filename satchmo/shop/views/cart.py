b00e3be627f8285287bfc03644b1c8aa4c178ca4
jshaffer <jshaffer@a38d40e9-c014-0410-b785-c606c0c8e7de>
2007-07-21 22:21:29 +0000

    Reformat shop/views/cart.py. Fix a bug where a user could add 0 of an item to the cart.
    git-svn-id: svn://satchmoproject.com/satchmo/trunk@557 a38d40e9-c014-0410-b785-c606c0c8e7de

--- satchmo/shop/views/cart.py
+++ satchmo/shop/views/cart.py
@@ -1,71 +1,73 @@
+from decimal import Decimal
+from sets import Set
+from django.conf import settings
+from django.core import urlresolvers
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
-from django import http
+from django.template import RequestContext, Context
 from django.utils.simplejson.encoder import JSONEncoder
+from django.utils.translation import ugettext as _
+from satchmo.shop.views.utils import bad_or_missing
 from satchmo.shop.models import Cart, CartItem, Config
 from satchmo.product.models import Product, ConfigurableProduct, Category, Option
 from satchmo.product.views import optionset_from_post
-from sets import Set
-from django.template import RequestContext, Context
-from django.conf import settings
-from satchmo.shop.views.utils import bad_or_missing
-from decimal import Decimal
 
 def display(request):
-    #Show the items in the cart
-    cart_list = []
+    """Display the items in the cart."""
     total = Decimal("0")
     all_items = []
-    if request.session.get('cart',False):
-        tempCart = Cart.objects.get(id=request.session['cart'])
-        total = tempCart.total
-        all_items = tempCart.cartitem_set.all()
-    return render_to_response('base_cart.html', {'all_items' : all_items,
-                                                 'total':total},
-                                                 RequestContext(request))
+    if request.session.get('cart'):
+        cart = Cart.objects.get(id=request.session['cart'])
+        total = cart.total
+        all_items = cart.cartitem_set.all()
+    context = RequestContext(request, {
+        'all_items': all_items, 'total': total})
+    return render_to_response('base_cart.html', context)
 
 def add(request, id=0):
-    #Todo: Error checking for invalid combos
-    #Add an item to the session/cart
+    """Add an item to the cart."""
+    #TODO: Error checking for invalid combos
 
     try:
         product = Product.objects.get(slug=request.POST['productname'])
         if 'ConfigurableProduct' in product.get_subtypes():
-            #This can happen if ajax isn't working to update productname
+            # This happens when productname cannot be updated by javascript.
             cp = product.configurableproduct
             chosenOptions = optionset_from_post(cp, request.POST)
             product = cp.get_product_from_options(chosenOptions)
     except Product.DoesNotExist:
-        return bad_or_missing(request, 'The product you have requested does '
-                'not exist.')
+        return bad_or_missing(request, _('The product you have requested ' +
+            'does not exist.'))
     try:
         quantity = int(request.POST['quantity'])
     except ValueError:
-        return render_to_response('base_product.html', {
+        context = RequestContext(request, {
             'item': product,
-            'error_message': "Please enter a whole number"},
-             RequestContext(request))
-    if quantity < 0:
-        return render_to_response('base_product.html', {
+            'error_message': _("Please enter a whole number.")})
+        return render_to_response('base_product.html', context)
+    if quantity < 1:
+        context = RequestContext(request, {
             'item': product,
-            'error_message': "Negative numbers can not be entered"},
-             RequestContext(request))
+            'error_message': _("Please enter a positive number.")})
+        return render_to_response('base_product.html', context)
 
-    if request.session.get('cart',False):
-        tempCart = Cart.objects.get(id=request.session['cart'])
+    if request.session.get('cart'):
+        cart = Cart.objects.get(id=request.session['cart'])
     else:
-        tempCart = Cart()
-    tempCart.save() #need to make sure there's an id
-    tempCart.add_item(product, number_added=quantity)
-    request.session['cart'] = tempCart.id
+        cart = Cart()
+        cart.save() # Give the cart an id
+    cart.add_item(product, number_added=quantity)
+    request.session['cart'] = cart.id
 
-    return http.HttpResponseRedirect('%s/cart/' % (settings.SHOP_BASE))
+    url = urlresolvers.reverse('satchmo_cart')
+    return HttpResponseRedirect(url)
 
 def add_ajax(request, id=0, template="json.html"):
     data = {'errors': []}
     try:
         product = Product.objects.get(slug=request.POST['productname'])
         if 'ConfigurableProduct' in product.get_subtypes():
-            #This can happen if ajax isn't working to update productname
+            # This happens when productname cannot be updated by javascript.
             cp = product.configurableproduct
             chosenOptions = optionset_from_post(cp, request.POST)
             product = cp.get_product_from_options(chosenOptions)
@@ -76,33 +78,36 @@ def add_ajax(request, id=0, template="json.html"):
         try:
             quantity = int(request.POST['quantity'])
             if quantity < 0:
-                data['errors'].append(['quantity',_('Choose a quantity')])
-            
+                data['errors'].append(('quantity', _('Choose a quantity')))
+
         except ValueError:
-            data['errors'].append(['quantity',_('Choose a whole number')])
-            
+            data['errors'].append(('quantity', _('Choose a whole number')))
+
     except Product.DoesNotExist:
-        data['errors'].append(['product', _('The product you have requested does not exist.')])
-        
-    if request.session.get('cart',False):
+        data['errors'].append(('product', _('The product you have requested does not exist.')))
+
+    if request.session.get('cart'):
         tempCart = Cart.objects.get(id=request.session['cart'])
     else:
         tempCart = Cart()
-        
-    if len(data['errors']) == 0:    
-        tempCart.save() #need to make sure there's an id
+        tempCart.save() # Give the cart an id
+
+    if not data['errors']:
         tempCart.add_item(product, number_added=quantity)
         request.session['cart'] = tempCart.id
         data['results'] = _('Success')
     else:
         data['results'] = _('Error')
-        
+
     data['cart_count'] = tempCart.numItems
 
     return render_to_response(template, {'json' : JSONEncoder().encode(data)})
-    
+
 def remove(request, id):
-    tempCart = Cart.objects.get(id=request.session['cart'])
-    quantity = int(request.POST.get('quantity','999999'))
-    tempCart.remove_item(id, quantity)
-    return http.HttpResponseRedirect('%s/cart/' % (settings.SHOP_BASE))
+    """Remove an item from the cart."""
+    item = CartItem.objects.get(id=id)
+    item.delete()
+
+    url = urlresolvers.reverse('satchmo_cart')
+    return HttpResponseRedirect(url)
+
