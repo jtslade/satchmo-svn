8846b4efa519d9c2f90fe304921b9ca2d2b201c0
chris <chris@a38d40e9-c014-0410-b785-c606c0c8e7de>
2007-09-08 12:33:55 +0000

    More updates to l10n.  More admin areas are in place.  Some model tweaks.
    git-svn-id: svn://satchmoproject.com/satchmo/trunk@700 a38d40e9-c014-0410-b785-c606c0c8e7de

--- satchmo/l10n/models.py
+++ satchmo/l10n/models.py
@@ -54,10 +54,10 @@ class Country(models.Model):
     name = models.CharField(_('Official name (CAPS)'), max_length=128)
     printable_name = models.CharField(_('Country name'), max_length=128)
     iso3_code = models.CharField(_('ISO alpha-3'), max_length=3, unique=True)
-    numcode = models.PositiveSmallIntegerField(_('ISO numeric'), null=True)
+    numcode = models.PositiveSmallIntegerField(_('ISO numeric'), null=True, blank=True)
     active = models.BooleanField(_('Country is active'), default=True)
     continent = models.CharField(_('Continent'), choices=CONTINENTS, max_length=2)
-    admin_area = models.CharField(_('Administrative Area'), choices=AREAS, max_length=2, null=True)
+    admin_area = models.CharField(_('Administrative Area'), choices=AREAS, max_length=2, null=True, blank=True)
     
     class Meta:
         verbose_name = _('Country')
@@ -79,8 +79,9 @@ class AdminArea(models.Model):
     Administrative Area level 1 for a country.  For the US, this would be the states
     """
     country = models.ForeignKey(Country, edit_inline=models.TABULAR)
-    name = models.CharField(_('Admin Area name'), max_length=50, core=True)
-    abbrev = models.CharField(_('Postal Abbreviation'), max_length=3, null=False)
+    name = models.CharField(_('Admin Area name'), max_length=60, core=True)
+    abbrev = models.CharField(_('Postal Abbreviation'), max_length=3, null=True, blank=True)
+    active = models.BooleanField(_('Area is active'), default=True)
 
     class Meta:
         verbose_name = _('Administrative Area')
