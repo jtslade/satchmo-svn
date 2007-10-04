fcaa5d5bfaa2bda10a599e39fd444b59390b1943
bkroeze <bkroeze@a38d40e9-c014-0410-b785-c606c0c8e7de>
2007-10-04 16:56:41 +0000

    adding some convenience functions for common caching use-cases
    git-svn-id: svn://satchmoproject.com/satchmo/trunk@801 a38d40e9-c014-0410-b785-c606c0c8e7de

--- satchmo/caching/models.py
+++ satchmo/caching/models.py
@@ -31,3 +31,34 @@ class CachedObjectMixin(object):
         
     def is_cached(self, *args, **kwargs):
         return caching.is_cached(self.cache_key(*args, **kwargs))
+
+def find_by_key(cls, groupkey, key):
+    """A helper function to look up an object by key"""
+    ob = None
+    try:
+        ob = caching.cache_get(groupkey, key)
+    except caching.NotCachedError, e:
+        try: 
+            ob = cls.objects.get(key__exact=key)
+            caching.cache_set(e.key, value=grp)
+
+        except cls.DoesNotExist:
+            log.debug("No such %s: %s", groupkey, key)
+
+    return ob
+    
+def find_by_slug(cls, groupkey, slug):
+    """A helper function to look up an object by slug"""
+    ob = None
+    try:
+        ob = caching.cache_get(groupkey, slug)
+    except caching.NotCachedError, e:
+        try: 
+            ob = cls.objects.get(slug__exact=slug)
+            caching.cache_set(e.key, value=grp)
+
+        except cls.DoesNotExist:
+            log.debug("No such %s: %s", groupkey, slug)
+
+    return ob
+
